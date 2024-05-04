09b986bda381a030f7a6390bbad5b6b6e83bbe76
chriswebdv <chriswebdv@gmail.com>
2024-05-04 10:19:15 -0600

    Capitalize first letter of string

---
+++ javascript-sandbox-start/01-variables-data-types/10-capitalize-challenge/script.js
@@ -0,0 +1,5 @@
+// Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
+const myString = 'developer';
+
+const myNewString = myString.charAt().toUpperCase()
+console.log(myNewString + myString.substring(1));
