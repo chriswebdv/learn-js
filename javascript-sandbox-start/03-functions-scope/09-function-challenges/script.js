abea46b298bd2b2436a4ba8ce074ac15ebf7dc75
chriswebdv <chriswebdv@gmail.com>
2024-05-04 12:51:27 -0600

    Create function that take arr as argument and return the min and max integers in the arr

--- javascript-sandbox-start/03-functions-scope/09-function-challenges/script.js
+++ javascript-sandbox-start/03-functions-scope/09-function-challenges/script.js
@@ -1,11 +1,12 @@
 // Celcius formula = (F - 32) * 5 / 9
 
-/*
+// Regular Function
 function getCelcius(temp) {
     result = (temp -32) * (5/9);
     return result;
 }
-*/
+
+// Arrow Function
 
 let myTemp = temp => (temp -32) * (5/9);
 
---
+++ javascript-sandbox-start/03-functions-scope/09-function-challenges/script2.js
@@ -0,0 +1,11 @@
+function minMax(arr) {
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+
+    return {
+        min,
+        max,
+    }
+}
+
+console.log(minMax([1,2,3,4,5,6,7,8,9]));
